4a944364761e626aed787dca59aa8e4f97062531
JPW <jpw@JPWs-MBP.attlocal.net>
2026-08-07 11:15:05 -0500

    Add Impact site verification

--- app/layout.tsx
+++ app/layout.tsx
@@ -7,5 +7,19 @@ import "./globals.css";
 export const metadata: Metadata = defaultMetadata("/");
 
 export default function RootLayout({ children }: Readonly<{ children: React.ReactNode }>) {
-  return <html lang="en" suppressHydrationWarning><body><FastPointerRainbow/>{children}<SiteFooter/></body></html>;
+  return (
+    <html lang="en" suppressHydrationWarning>
+      <head>
+        <meta
+          name="impact-site-verification"
+          {...{ value: "2234f842-3095-4ddb-9262-6807be6c9a16" }}
+        />
+      </head>
+      <body>
+        <FastPointerRainbow />
+        {children}
+        <SiteFooter />
+      </body>
+    </html>
+  );
 }
