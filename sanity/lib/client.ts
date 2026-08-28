a21c42d51cdafc1db4de66c42dfe2681d811ce8b
JPW <jpw@JPWs-MBP.attlocal.net>
2026-08-28 14:36:57 -0500

    Use production Sanity project by default

--- sanity/lib/client.ts
+++ sanity/lib/client.ts
@@ -1,7 +1,7 @@
 import { createClient } from "next-sanity";
 
-export const projectId=process.env.NEXT_PUBLIC_SANITY_PROJECT_ID;
+export const projectId=process.env.NEXT_PUBLIC_SANITY_PROJECT_ID||"8luodcfj";
 export const dataset=process.env.NEXT_PUBLIC_SANITY_DATASET||"production";
 export const apiVersion=process.env.NEXT_PUBLIC_SANITY_API_VERSION||"2025-01-01";
-export const sanityConfigured=Boolean(projectId);
-export const client=createClient({ projectId:projectId||"8luodcfj",dataset,apiVersion,useCdn:false,perspective:"published" });
+export const sanityConfigured=true;
+export const client=createClient({ projectId,dataset,apiVersion,useCdn:false,perspective:"published" });
