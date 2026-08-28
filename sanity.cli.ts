e277298c07c74c1a600f824700a97c0dd0d04371
JPW <jpw@JPWs-MacBook-Pro.local>
2026-08-28 14:06:53 -0500

    Configure hosted Sanity Studio

---
+++ sanity.cli.ts
@@ -0,0 +1,9 @@
+import {defineCliConfig} from "sanity/cli";
+
+export default defineCliConfig({
+  studioHost:"smallcertainty",
+  api:{
+    projectId:"8luodcfj",
+    dataset:"production",
+  },
+});
